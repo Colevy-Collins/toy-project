cfccf81ed0b71799f1aa7e57163f8f0c31efaffe
Colevy Collins <colevy5111@icloud.com>
2023-04-03 17:03:31 -0400

    colevy-added jest and integ test for update

---
+++ Todoapps1/index.test.js
@@ -0,0 +1,25 @@
+const request = require("supertest")
+const baseURL = "http://localhost:5500"
+
+describe("POST /update", () => {
+      const newTodo = {
+        _id : 1,
+        title : "test1",
+        date : "now",
+      }
+      const newTodo2 = {
+            _id : 1,
+            title : "test1",
+            date : "later",
+          }
+      beforeAll(async () => {
+        await request(baseURL).post(`/add/${newTodo.id}`)
+      })
+    afterAll(async () => {
+            await request(baseURL).delete(`/delete/${newTodo.id}`)
+    })
+      it("should add an item to todos array", async () => {
+        const response = await request(baseURL).post("/update").send(newTodo2);
+        expect(response.text).toBe('<h1 style="text-align:center">Stored to MongoDB</h1><br/><a style="text-decoration:none; color:black;  text-align:center" href="/list"><div style="border:1px solid black;"><h2 style="">Return to list</h2></div></a>');
+      });
+    });
