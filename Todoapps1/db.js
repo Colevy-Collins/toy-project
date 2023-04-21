8107cbd29c4d789875568365fa8379b81629723a
Colevy Collins <colevy5111@icloud.com>
2023-04-21 11:22:35 -0400

    removed node modulres

--- Todoapps1/db.js
+++ Todoapps1/db.js
@@ -2,7 +2,7 @@ const ID='user1';
 const PASSWORD = 'wtYMh4ErXkzBcyD1'; 
 const NET='asecourses.c78gwho.mongodb.net/asecourses';
 
-// Connection URI
+// Connection URI 
 const uri = `mongodb+srv://${ID}:${PASSWORD}@${NET}/?retryWrites=true&w=majority`
 //const connectStr = "mongodb+srv://user1:wtYMh4ErXkzBcyD1@asecourses.c78gwho.mongodb.net/asecourses";
 
